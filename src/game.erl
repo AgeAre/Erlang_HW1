888d3b70cab6ac1e3451419275659a2daaa43ee5
AgeAre <agearehr@gmail.com>
2019-05-01 21:07:56 +0300

    Submitted - working well

--- src/game.erl
+++ src/game.erl
@@ -12,7 +12,6 @@
 %% API
 -export([canWin/1, nextMove/1, explanation/0]).
 
-% Assuming that the corner case of N = 0 means that the current player has no more matches to take, so he loses.
 %canWin(0) -> false;
 canWin(1) -> true;
 canWin(2) -> true;
@@ -20,7 +19,7 @@ canWin(N) when (N > 0), (is_integer(N)) ->
   not canWin(N - 1) or not canWin(N - 2).
 
 
-
+%nextMove(0) -> false;
 nextMove(1) -> {true, 1};
 nextMove(2) -> {true, 2};
 nextMove(3) -> false;
@@ -32,8 +31,9 @@ nextMove(N) when (N > 0), (is_integer(N))->
         true -> {true, 2};
         false -> false
       end
-    end
+  end
 .
 
-explanation() ->
-  io:format("Answer!!!!!!!!!!!!!!!!!!!").
+explanation() -> {"The difficulty to provide a tail recursion solution is that it's not that intuitive to do so. "
+                  "We've seen in class that tail recurcion is possible, by solving a similar problem (Fibonacci series)"
+                  " that has the same recursive solution and the same boundary values problem."}.
